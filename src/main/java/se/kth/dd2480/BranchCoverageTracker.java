b824a21fe2627713778a476a847e2d77a87e03d0
Martin Catheland <m.catheland@gmail.com>
2024-02-21 13:03:07 +0100

    feat: add one line ternary operator coverage compatibility and adding string to coverage property (#7)

--- src/main/java/se/kth/dd2480/BranchCoverageTracker.java
+++ src/main/java/se/kth/dd2480/BranchCoverageTracker.java
@@ -22,7 +22,7 @@ public class BranchCoverageTracker {
      * <code>BranchCoverageTracker.addCoverage();</code>
      */
     public static void addCoverage() {
-        generateKey();
+        generateKey("");
     }
 
 
@@ -30,28 +30,52 @@ public class BranchCoverageTracker {
      * Also add line coverage to the coverage map adding the possibility to return a value.
      * This can be used to add coverage to some constructs like ternary operators.
      * <br><br>
-     * <i>Usage with ternary operator :</i>
+     * <i>Usage with ternary operator (need to call this method on different lines) :</i>
+     * :</i>
      * <br>
-     * <code>int result = condition ? BranchCoverageTracker.addCoverage(1) :
-     * BranchCoverageTracker.addCoverage(0);</code>
+     * <code>int result = condition ?</code>
+     * <br><code>BranchCoverageTracker.addCoverage(1) :</code>
+     * <br><code>BranchCoverageTracker.addCoverage(2);</code>
      * <br><br>
      * @param object Any object to be returned
      * @return the object passed as parameter
      * @param <T> Any type of object
      */
     public static <T> T addCoverage(T object) {
-        generateKey();
+        generateKey("");
+        return object;
+    }
+
+
+    /**
+     * Similar to the previous method, but with the possibility to add a string to the key.
+     * This can be used to add coverage to some constructs like ternary
+     * operators on a single line.
+     * <br><br>
+     * <i>Usage with ternary operator :</i>
+     * <br>
+     * <code>int result = condition ? addCoverage(1, "1") : addCoverage(2, "2");</code>
+     * @param object Any object to be returned
+     * @param toAdd A string to be added to the key
+     * @return the object passed as parameter
+     * @param <T> Any type of object
+     */
+    public static <T> T addCoverage(T object, String toAdd) {
+        generateKey(toAdd);
         return object;
     }
 
     /**
      * Generate a key for the coverage map. The key is generated based on the class name, method name and line number.
      */
-    private static void generateKey() {
+    private static void generateKey(String toAdd) {
         StackTraceElement caller = Thread.currentThread().getStackTrace()[3];
         String className = caller.getClassName().split("\\.")[caller.getClassName().split("\\.").length - 1];
         String key =
                 className + "." + caller.getMethodName() + "." + caller.getLineNumber();
+        if (!toAdd.isEmpty()) {
+            key += "." + toAdd;
+        }
         if (coverageMap.put(key, true) == null) {
             CoverageSaver.saveCoverage(coverageMap);
         }
