f0202b325cbb4ea6382f022d352356bf8d8d65be
Martin Catheland <m.catheland@gmail.com>
2024-02-21 12:39:25 +0100

    feat: add ternary operator coverage compatibility (#7)

--- src/main/java/se/kth/dd2480/BranchCoverageTracker.java
+++ src/main/java/se/kth/dd2480/BranchCoverageTracker.java
@@ -7,10 +7,6 @@ import java.util.Map;
 /**
  * <h3>BranchCoverageTracker</h3>
  * This class is used to track code coverage for the program.
- * <br>
- * <i>Usage :</i> Add the following line every time you want to track a branch coverage:
- * <br>
- * <code>BranchCoverageTracker.addCoverage();</code>
  */
 public class BranchCoverageTracker {
     /**
@@ -20,9 +16,39 @@ public class BranchCoverageTracker {
 
     /**
      * Add line coverage to the coverage map. This method should be called every time a branch is taken.
+     * <br><br>
+     * <i>Usage :</i> Add the following line every time you want to track a branch coverage:
+     * <br>
+     * <code>BranchCoverageTracker.addCoverage();</code>
      */
     public static void addCoverage() {
-        StackTraceElement caller = Thread.currentThread().getStackTrace()[2];
+        generateKey();
+    }
+
+
+    /**
+     * Also add line coverage to the coverage map adding the possibility to return a value.
+     * This can be used to add coverage to some constructs like ternary operators.
+     * <br><br>
+     * <i>Usage with ternary operator :</i>
+     * <br>
+     * <code>int result = condition ? BranchCoverageTracker.addCoverage(1) :
+     * BranchCoverageTracker.addCoverage(0);</code>
+     * <br><br>
+     * @param object Any object to be returned
+     * @return the object passed as parameter
+     * @param <T> Any type of object
+     */
+    public static <T> T addCoverage(T object) {
+        generateKey();
+        return object;
+    }
+
+    /**
+     * Generate a key for the coverage map. The key is generated based on the class name, method name and line number.
+     */
+    private static void generateKey() {
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[3];
         String className = caller.getClassName().split("\\.")[caller.getClassName().split("\\.").length - 1];
         String key =
                 className + "." + caller.getMethodName() + "." + caller.getLineNumber();
